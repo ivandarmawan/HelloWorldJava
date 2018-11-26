67cd29bb2d7369f45aa53659d96f4ca2deb41612
Ivan Darmawan <ivandarmawan@users.noreply.github.com>
2018-11-26 14:20:42 +0700

    Hello Conflict - Ivan

--- src/helloworldjava/HelloWorldJava.java
+++ src/helloworldjava/HelloWorldJava.java
@@ -17,6 +17,7 @@ public class HelloWorldJava {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi, I am Fathira. Commited from Netbeans");
+        System.out.println("Hello Conflict - Ivan");
     }
     
 }
